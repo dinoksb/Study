634a277dbb0911bac0057c2349f08a1b5f34a059
SeungBaeKim <sb.kim@morph.kr>
2017-09-11 18:27:48 +0900

    Screenshot 및 Unity_AndroidSTTPlugin

---
+++ Unity_AndroidSTTPlugin/AndroidPluginCall.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidPluginCall : MonoBehaviour
+{
+    public Text resultText;
+    public Text debugText;
+
+    private void Awake()
+    {
+        AndroidPluginManager.getInstance();
+    }
+
+    private void Start()
+    {
+        AndroidPluginManager.getInstance().SetCallbackSTTresult(new delegateSTTResult(STTResult));
+        AndroidPluginManager.getInstance().SetCallbackMessage(new delegateMessage(AndroidMessage));
+    }
+
+    private void STTResult(string result)
+    {
+        Debug.Log("STT : " + result);
+        resultText.text = result;
+    }
+
+    private void AndroidMessage(string msg)
+    {
+        Debug.Log("Log: " + msg);
+        debugText.text = msg;
+    }
+
+    /// <음성인식>
+    /// 여기서 호출하면 안드로이드에서 실제 음성인식 호출하고 등록된 리스너가 
+    /// 처리를 해서 위에 콜백으로 결과가 들어오게 됩니다.
+    /// </음성인식>
+    public void Call_STTstart()
+    {
+        AndroidPluginManager.getInstance().Call_AndroidSTT("ko-KR");
+    }
+}
---
+++ Unity_AndroidSTTPlugin/AndroidPluginManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public delegate void delegateSTTResult(string result);
+public delegate void delegateMessage(string msg);
+
+public class AndroidPluginManager : MonoBehaviour
+{
+    private static AndroidPluginManager _instance;
+    private static GameObject content;
+    public static AndroidPluginManager getInstance()
+    {
+        if (_instance == null)
+        {
+            content = new GameObject();
+            content.name = "pluginUnity"; // 오브젝트 이름 틀리면 안됩니다.
+            _instance = content.AddComponent(typeof(AndroidPluginManager)) as AndroidPluginManager;
+        }
+        return _instance;
+    }
+
+    private AndroidJavaObject AJObject = null;
+    private AndroidJavaClass AJClass = null;
+
+    private delegateSTTResult sttResult;
+    private delegateMessage message;
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+
+    private void Awake()
+    {
+        Debug.Log("AJOject_0: " + AJObject);
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+        AJClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AJObject = AJClass.GetStatic<AndroidJavaObject>("currentActivity");
+#endif
+
+        Debug.Log("AJOject_1: " + AJObject);
+    }
+
+    // 안드로이드에 있는 함수를 호출.
+    public void Call_AndroidSTT(string _lang)
+    {
+        AJObject.Call("StartSpeechReco", _lang);
+    }
+
+    // 안드로이드 메세지.
+    public void msgUnity(string _msg)
+    {
+        if(message != null)
+        {
+            message(_msg);
+        }
+    }
+
+    // 음성인식 결과.
+    public void sttUnity(string _result)
+    {
+        if(sttResult != null)
+        {
+            sttResult(_result);
+        }
+    }
+
+    // 받은것을 콜백으로 실제 사용하는 스크립트로 넘겨준다.
+    public void SetCallbackSTTresult(delegateSTTResult callback) { this.sttResult = callback; }
+    public void SetCallbackMessage(delegateMessage callback) { this.message = callback; }
+
+}
