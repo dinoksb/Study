16be93ab05a1752a96d8885336e95c0f124fa42e
SeungBaeKim <sb.kim@morph.kr>
2020-03-02 17:25:45 +0900

    Unity add symbol via script

---
+++ UnityTips/DefineSymbol/SymbolCustom.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+namespace Assets.Editor
+{
+    static class SymbolCustom
+    {
+        private static IEnumerable<string> currentSymbols;
+
+        private static BuildTargetGroup[] buildTargetGroup = null;
+
+        public static string CurrentSymbols
+        {
+            get { return String.Join(";", currentSymbols.ToArray()); }
+        }
+
+        static DefineSymbol()
+        {
+            currentSymbols = PlayerSettings.GetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup).Split(';');
+        }
+
+        private static void SaveSymbol()
+        {
+            var symbols = CurrentSymbols;
+            foreach (var target in buildTargetGroup)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(target, symbols);
+            }
+        }
+
+
+
+        public static void Add(BuildTargetGroup[] buildTargetGroups, params string[] symbols)
+        {
+            buildTargetGroup = buildTargetGroups;
+            currentSymbols = currentSymbols.Concat(symbols).Distinct().ToArray();
+
+            SaveSymbol();
+        }
+
+        public static void Remove(BuildTargetGroup[] buildTargetGroups, params string[] symbols)
+        {
+            buildTargetGroup = buildTargetGroups;
+            currentSymbols = currentSymbols.Except(symbols).ToArray();
+
+            SaveSymbol();
+        }
+
+        public static void Set(params string[] symbols)
+        {
+            currentSymbols = symbols;
+            SaveSymbol();
+        }
+    }
+}
